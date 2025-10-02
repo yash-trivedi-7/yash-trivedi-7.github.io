864e2e24020eaf199fd36d7a65589c5d637194ce
Yash <yashtrivedi7.ca@gmail.com>
2025-10-02 03:37:14 -0400

    Update typing cursor.js

--- typing cursor.js
+++ typing cursor.js
@@ -1,6 +1,6 @@
  document.addEventListener("DOMContentLoaded", function() {
     new Typed("#typed-name", {
-      strings: ["Yash Trivedi"],   // you can add more like ["Yash Trivedi", "a Project Manager"]
+      strings: ["I am Yash Trivedi"],   // you can add more like ["Yash Trivedi", "a Project Manager"]
       typeSpeed: 80,              // typing speed
       backSpeed: 50,              // backspacing speed (if multiple strings)
       startDelay: 300,            // delay before typing starts
