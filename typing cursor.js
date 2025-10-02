52c81af85f8821db31b9bdff3b7bb2d9905d4281
yash-trivedi-7 <yashtrivedi7.ca@gmail.com>
2025-10-02 02:01:00 -0400

    Create typing cursor.js

---
+++ typing cursor.js
@@ -0,0 +1,11 @@
+ document.addEventListener("DOMContentLoaded", function() {
+    new Typed("#typed-name", {
+      strings: ["Yash Trivedi"],   // you can add more like ["Yash Trivedi", "a Project Manager"]
+      typeSpeed: 80,              // typing speed
+      backSpeed: 50,              // backspacing speed (if multiple strings)
+      startDelay: 300,            // delay before typing starts
+      showCursor: true,           // blinking cursor
+      cursorChar: "|",            // cursor character
+      loop: false                 // set to true if you want it to keep cycling
+    });
+  });
